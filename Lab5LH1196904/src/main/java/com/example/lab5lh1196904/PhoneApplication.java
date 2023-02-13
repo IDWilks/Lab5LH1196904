45f825a71abcbd2db3f4397d46c44566e630036a
IDWilks <122938132+IDWilks@users.noreply.github.com>
2023-02-13 01:16:44 -0500

    initial commit
    add files up to step 16 of in class tutorial

---
+++ Lab5LH1196904/src/main/java/com/example/lab5lh1196904/PhoneApplication.java
@@ -0,0 +1,23 @@
+package com.example.lab5lh1196904;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PhoneApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(PhoneApplication.class.getResource("Phone-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Apple IPhone");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Lab5LH1196904/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.lab5lh1196904 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.lab5lh1196904 to javafx.fxml;
+    exports com.example.lab5lh1196904;
+}
